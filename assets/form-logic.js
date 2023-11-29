b33e0c77a8cc6c953f003005f55ba9abb81a86f9
William Banks <williamjb555@gmail.com>
2023-11-29 14:14:57 +0000

    Added basic task management (but tasks can't be removed)

---
+++ assets/form-logic.js
@@ -0,0 +1,73 @@
+let task_forms = document.querySelectorAll("form.task-form");
+let task_inputs = new Map();
+
+for (let i = 0; i < task_forms.length; i++) {
+	let form = task_forms[i];
+	let input_field = form.querySelector("input[type=text]");
+	task_inputs.set(form.getAttribute("id"), input_field.value);
+	form.addEventListener("submit", (e) => {
+		set_task(form);
+		e.preventDefault();
+	})
+}
+
+function task_on_change(input) {
+	let form = input.parentNode;
+	if(task_inputs.get(form.getAttribute("id")) != input.value) {
+		if(!input.classList.contains("task-updated")) {
+			input.classList.add("task-updated");
+		}
+	} else {
+		if(input.classList.contains("task-updated")) {
+			input.classList.remove("task-updated");
+		}
+	}
+}
+
+async function set_task(form) {
+	let form_data = new FormData(form);
+
+	if(task_inputs.get(form.getAttribute("id")) != form_data.get("task")) {
+		let icon_elem = form.querySelector(".material-symbols-outlined");
+		icon_elem.innerHTML = "progress_activity";
+		icon_elem.classList.add("spin");
+
+		fetch("/set_task", { method: "POST", body: form_data, redirect: "manual" })
+			.then((_) => {
+				icon_elem.innerHTML = "done";
+				icon_elem.classList.remove("spin");
+				task_inputs.set(form.getAttribute("id"), form_data.get("task"));
+				task_on_change(form.querySelector("input[type=text]"));
+			})
+	}
+}
+
+function add_task() {
+	let id = 0;
+
+	let task_forms = document.querySelectorAll("form.task-form");
+
+	for (let i = 0; i < task_forms.length; i++) {
+		const form = task_forms[i];
+		let fid_str = form.getAttribute("id");
+		let fid = parseInt(fid_str.slice(5));
+		if(fid >= id) {
+			id = fid + 1;
+		}
+	}
+
+	let task_elem = document.querySelector("#task_template").content.cloneNode(true);
+	let task_form = task_elem.querySelector("form");
+	let task_hidden_input = task_elem.querySelector("input[type=hidden]");
+
+	let task_id = `task-${id}`
+	task_form.setAttribute("id", task_id);
+	task_hidden_input.value = task_id;
+
+	let tasks_area = document.querySelector("main > section");
+	tasks_area.appendChild(task_elem);
+	task_form.addEventListener("submit", (e) => {
+		set_task(task_form);
+		e.preventDefault();
+	})
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
-use rocket::{launch, routes, get, fs::{FileServer, Options}};
+use std::{collections::HashMap, sync::RwLock, ops::Deref};
+
+use rocket::{launch, routes, get, fs::{FileServer, Options}, post, FromForm, form::Form, response::Redirect, uri, State};
 use rocket_dyn_templates::{Template, context};
+
+#[derive(FromForm, Debug)]
+struct TaskForm<'a> {
+    id: &'a str,
+    task: &'a str,
+}
+
+struct Tasks {
+    pub tasks: RwLock<HashMap<String, String>>
+}
 
 #[get("/")]
 fn index() -> Template {
@@ -12,8 +24,22 @@ fn login() -> Template {
 }
 
 #[get("/tasks")]
-fn tasks() -> Template {
-    Template::render("tasks", context! {})
+fn tasks(tasks: &State<Tasks>) -> Template {
+    let tasks = tasks.inner().tasks.read().unwrap();
+    Template::render("tasks", context! { tasks: tasks.deref() })
+}
+
+#[post("/set_task", data = "<task_form>")]
+fn set_task(task_form: Form<TaskForm<'_>>, tasks: &State<Tasks>) -> Redirect {
+    println!("Recieved task data: {:?}", task_form);
+    tasks.inner().tasks.write().unwrap().insert(task_form.id.to_string(), task_form.task.to_string());
+    Redirect::to(uri!("/tasks"))
+}
+
+#[post("/remove_task", data = "<task_form>")]
+fn remove_task(task_form: Form<TaskForm<'_>>, tasks: &State<Tasks>) -> Redirect {
+    tasks.inner().tasks.write().unwrap().remove(&task_form.id.to_string());
+    Redirect::to(uri!("/tasks"))
 }
 
 #[get("/signup")]
@@ -24,7 +50,8 @@ fn signup() -> Template {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index, tasks, signup, login])
+        .mount("/", routes![index, tasks, set_task, remove_task, signup, login])
         .mount("/assets/", FileServer::new("assets", Options::None))
         .attach(Template::fairing())
+        .manage(Tasks { tasks: RwLock::new(HashMap::new()) })
 }
