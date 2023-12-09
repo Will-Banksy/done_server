da0cae8125d1ecbd28adec5e3115dadc3c88bbba
Will Banks <williamjb555@gmail.com>
2023-12-09 21:17:15 +0000

    Corrections to AWS setup script

--- aws_dev_setup.sh
+++ aws_dev_setup.sh
@@ -10,7 +10,7 @@ region="us-east-1";
 
 aws rds create-db-instance --db-instance-identifier $db_instance_name --db-instance-class db.t3.micro --engine mariadb --master-username $master_user --master-user-password $master_pass --allocated-storage 20 --publicly-accessible --backup-retention-period 0 --region $region;
 
-sgid=$(aws rds describe-db-instances --db-instance-identifier sboxdb --region $region | awk '/VpcSecurityGroupId/ { print $2 }' | tr -d '",');
+sgid=$(aws rds describe-db-instances --db-instance-identifier $db_instance_name --region $region | awk '/VpcSecurityGroupId/ { print $2 }' | tr -d '",');
 
 sgrid=$(aws ec2 describe-security-group-rules --filters Name="group-id",Values="$sgid" --region $region | awk '/SecurityGroupRuleId/ { sgrid = $2 } /"IsEgress": false/ { print sgrid }' | tr -d '",');
 
@@ -20,6 +20,6 @@ aws ec2 revoke-security-group-ingress --group-id "$sgid" --security-group-rule-i
 
 aws ec2 authorize-security-group-ingress --group-id "$sgid" --ip-permissions "IpProtocol"="-1","FromPort"=-1,"ToPort"=-1,"IpRanges"="[{CidrIp=${pub_ip}/32}]" --region $region;
 
-aws rds wait db-instance-available --db-instance-identifier sboxdb --region $region;
+aws rds wait db-instance-available --db-instance-identifier $db_instance_name --region $region;
 
 echo "Amazon RDS database instance is now available!";
